827ac86ca299489608f9a13e554528be66cbbebd
Nashrul Fikri <nashrul.fikri19@gmail.com>
2026-01-02 14:24:39 +0700

    Setting Graddle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    // Perbaikan: Pakai titik (.) bukan minus (-)
     alias(libs.plugins.google.services) apply false
 }
