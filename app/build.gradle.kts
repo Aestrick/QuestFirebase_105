7aec5cf3fed4638f3d178a7d996f45fb8f9dadc3
Nashrul Fikri <nashrul.fikri19@gmail.com>
2026-01-02 14:24:21 +0700

    Setting Graddle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,18 +2,18 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    // Perbaikan: Pakai titik (.)
+    alias(libs.plugins.google.services)
 }
 
 android {
-    namespace = "com.example.pertemuan13"
-    compileSdk {
-        version = release(36)
-    }
+    namespace = "com.example.pertemuan13" // Sesuaikan dengan nama packagemu
+    compileSdk = 35 // Atau 34, sesuaikan default Android Studio
 
     defaultConfig {
         applicationId = "com.example.pertemuan13"
-        minSdk = 29
-        targetSdk = 36
+        minSdk = 24
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -46,15 +46,28 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
+    // --- DEPENDENCIES BARU ---
+    // Navigasi
+    implementation(libs.androidx.navigation.compose)
+
+    // Firebase (BOM mengatur versi otomatis)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.analytics)
+
+    // Coroutines Play Services (Agar bisa pakai .await() di Firebase)
+    implementation(libs.coroutines.play.services)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
