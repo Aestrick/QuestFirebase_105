f4129d791aa0913d24490434db8f078711c2f993
Nashrul Fikri <nashrul.fikri19@gmail.com>
2026-01-02 14:48:54 +0700

    nambahin PenyediaViewModel.kt

---
+++ app/src/main/java/com/example/pertemuan13/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.pertemuan13.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pertemuan13.repositori.AplikasiDataSiswa
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositorySiswa)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositorySiswa)
+        }
+
+        initializer {
+            DetailViewModel(
+                createSavedStateHandle(),
+                aplikasiSiswa().container.repositorySiswa
+            )
+        }
+
+        initializer {
+            EditViewModel(
+                createSavedStateHandle(),
+                aplikasiSiswa().container.repositorySiswa
+            )
+        }
+    }
+}
+
+fun CreationExtras.aplikasiSiswa(): AplikasiDataSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
